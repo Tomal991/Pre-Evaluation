fa228d9da6c4e18cbecb1a5265a9a9a5c6d55395
Tomal991 <intesarulhaquey4@gmail.com>
2021-07-23 00:06:16 +0600

    third commit

--- preEvaluation-1/src/main/java/com/example/demo/controller/Controlller.java
+++ preEvaluation-1/src/main/java/com/example/demo/controller/Controlller.java
@@ -20,110 +20,133 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.repository.Repository;
 import com.example.demo.user.Student;
+
 @CrossOrigin
 @RestController
 @RequestMapping("/api")
 public class Controlller {
 
-    @Autowired
-    Repository repository;
+	@Autowired
+	Repository repository;
 
-    @GetMapping("/student")
-    public ResponseEntity<List<Student>> getAllStudent(@RequestParam(required = false) Integer id) {
-        try {
-            List<Student> student = new ArrayList<Student>();
+	@GetMapping("/student")
+	public ResponseEntity<List<Student>> getAllStudent(@RequestParam(required = false) Integer id) {
+		try {
+			List<Student> student = new ArrayList<Student>();
 
-            if (id == null)
-            	repository.findAll().forEach(student::add);
-            else
-            	repository.findById(id).forEach(student::add);
+			if (id == null)
+				repository.findAll().forEach(student::add);
+			else
+				repository.findById(id).forEach(student::add);
 
-            if (student.isEmpty()) {
-                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-            }
+			if (student.isEmpty()) {
+				return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+			}
 
-            return new ResponseEntity<>(student, HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
+			return new ResponseEntity<>(student, HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 
-    @GetMapping("/student/{id}")
-    public ResponseEntity<Student> getStudentById(@PathVariable("id") long id) {
-        Optional<Student> student = repository.findById(id);
+	@GetMapping("/student/{id}")
+	public ResponseEntity<Student> getStudentById(@PathVariable("id") long id) {
+		Optional<Student> student = repository.findById(id);
 
-        if (student.isPresent()) {
-            return new ResponseEntity<>(student.get(), HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-    }
+		if (student.isPresent()) {
+			return new ResponseEntity<>(student.get(), HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
 
-    @PostMapping("/student")
-    public ResponseEntity<Student> createStudent(@RequestBody Student student) {
-        try {
-        	Student _student = repository
-                     .save(new Student(student.getRegId(),
-                    		 student.getIntitutionId(),
-                    		 student.getStudentId(),
-                    		 student.getFirstName(), null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null
-                    		 
-                    		 ));
-            return new ResponseEntity<>(_student, HttpStatus.CREATED);
-        } catch (Exception e) {
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
+	@PostMapping("/student")
+	public ResponseEntity<Student> createStudent(@RequestBody Student student) {
+		try {
+			Student _student = repository.save(new Student(student.getRegId(), student.getIntitutionId(),
+					student.getStudentId(), student.getFirstName(), student.getLastName(), student.getRegdate(),
+					student.getDateofBirth(), student.getAge(), student.getGender(), student.getNid(),
+					student.getOccupation(), student.getStreet(), student.getPostOffice(), student.getUpazilla(),
+					student.getDistrict(), student.getDivision(), student.getContact(), student.getEmail(),
+					student.getCreated_on(), student.getCreated_by(), student.getDegree(), student.getInstitute(),
+					student.getCompletion_year(), student.getGrade()
 
-    @PutMapping("/student/{id}")
-    public ResponseEntity<Student> updateStudent(@PathVariable("id") long id, @RequestBody Student student) {
-        Optional<Student> tutorialData = repository.findById(id);
+			));
+			return new ResponseEntity<>(_student, HttpStatus.CREATED);
+		} catch (Exception e) {
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 
-        if (tutorialData.isPresent()) {
-        	Student _student = tutorialData.get();
-        	_student.setRegId(student.getRegId());
-        	_student.setFirstName(student.getFirstName());
-        	_student.setStudentId(student.getStudentId());
-            return new ResponseEntity<>(repository.save(_student), HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-    }
+	@PutMapping("/student/{id}")
+	public ResponseEntity<Student> updateStudent(@PathVariable("id") long id, @RequestBody Student student) {
+		Optional<Student> tutorialData = repository.findById(id);
 
-    @DeleteMapping("/student/{id}")
-    public ResponseEntity<HttpStatus> deletestudent(@PathVariable("id") long id) {
-        try {
-            repository.deleteById(id);
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
+		if (tutorialData.isPresent()) {
+			Student _student = tutorialData.get();
+			_student.setRegId(student.getRegId());
+			_student.setFirstName(student.getFirstName());
+			_student.setStudentId(student.getStudentId());
 
-    @DeleteMapping("/student")
-    public ResponseEntity<HttpStatus> deleteAllStudent() {
-        try {
-            repository.deleteAll();
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+			_student.setLastName(student.getLastName());
+			_student.setRegdate(student.getRegdate());
+			_student.setDateofBirth(student.getDateofBirth());
+			_student.setAge(student.getAge());
+			_student.setGender(student.getGender());
+			_student.setNid(student.getNid());
+			_student.setOccupation(student.getOccupation());
+			_student.setStreet(student.getStreet());
+			_student.setPostOffice(student.getPostOffice());
+			_student.setUpazilla(student.getUpazilla());
+			_student.setDistrict(student.getDistrict());
+			_student.setDivision(student.getDivision());
+			_student.setContact(student.getContact());
+			_student.setEmail(student.getEmail());
+			_student.setCreated_on(student.getCreated_on());
+			_student.setCreated_by(student.getCreated_by());
+			_student.setDegree(student.getDegree());
+			_student.setInstitute(student.getInstitute());
+			_student.setCompletion_year(student.getCompletion_year());
+			_student.setGrade(student.getGrade());
+			return new ResponseEntity<>(repository.save(_student), HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
 
-    }
+	@DeleteMapping("/student/{id}")
+	public ResponseEntity<HttpStatus> deletestudent(@PathVariable("id") long id) {
+		try {
+			repository.deleteById(id);
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 
-    @GetMapping("/student/name/{firstName}")
-    public ResponseEntity<List<Student>> findByFirstNameContaining() {
-        try {
-            List<Student> student = repository.findByFirstNameContaining(null);
+	@DeleteMapping("/student")
+	public ResponseEntity<HttpStatus> deleteAllStudent() {
+		try {
+			repository.deleteAll();
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 
-            if (student.isEmpty()) {
-                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-            }
-            return new ResponseEntity<>(student, HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
+	}
+
+	@GetMapping("/student/name/{firstName}")
+	public ResponseEntity<List<Student>> findByFirstNameContaining() {
+		try {
+			List<Student> student = repository.findByFirstNameContaining(null);
+
+			if (student.isEmpty()) {
+				return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+			}
+			return new ResponseEntity<>(student, HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 
 }
-
